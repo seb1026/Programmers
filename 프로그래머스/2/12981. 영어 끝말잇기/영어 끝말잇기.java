dbd635a12f24a02add10c1d6a333b0a3d88d07cd
seb1026 <47293040+seb1026@users.noreply.github.com>
2024-01-31 00:11:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(words[0], 0);
+        
+        for(int i=1; i<words.length; i++) {
+            map.put(words[i],0);
+            if(words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0) || map.size() < i+1) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
