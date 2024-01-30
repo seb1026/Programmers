d6e6f754bfb668dbd4d1df6246e486334c816340
seb1026 <47293040+seb1026@users.noreply.github.com>
2024-01-31 00:23:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        long[] answer = new long[n];
+        answer[0] = 1;
+        if(n != 1) answer[1] = 2;
+        
+        for(int i=2; i<n; i++) {
+            answer[i] = (answer[i-1] + answer[i-2]) % 1234567;
+        }
+        
+        return answer[n-1];
+    }
+}
