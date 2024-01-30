c8a06903b90a51a632f197a5c51fb7984f889870
seb1026 <47293040+seb1026@users.noreply.github.com>
2024-01-31 00:05:10 +0900

    [level 1] Title: 과일 장수, Time: 0.45 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        Arrays.sort(score);
+        
+        for(int i=0; i<score.length/m; i++) {
+            answer += score[score.length-(i+1)*m] * m;
+        }
+        
+        return answer;
+    }
+}
