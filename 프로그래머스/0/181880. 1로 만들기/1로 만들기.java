fa38d90d7da032de594371700b6a3e346d21213c
seb1026 <47293040+seb1026@users.noreply.github.com>
2025-10-13 14:09:31 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        for(int i=0; i<num_list.length; i++) {
+            int num = num_list[i];
+            while(num > 1) {
+                num = num / 2;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
